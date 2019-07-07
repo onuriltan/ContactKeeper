b159b8d4b2d4b5a4120c6db47fb0ecda0c405a43
onuriltan <onur.iltan@gmail.com>
2019-07-07 18:38:31 +0300

    add mongodb connection

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect MongoDB
+connectDB();
 
 // Routes
 app.use('/api/users', require('./routes/users'));
