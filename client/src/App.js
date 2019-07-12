3bb7199c244ade8ea76ffbe295fec0e25873722e
onuriltan <onur.iltan@gmail.com>
2019-07-12 16:21:11 +0300

    add contact context

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,25 @@ import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import './App.css';
 import Home from './components/pages/Home'
 import About from './components/pages/About'
-
 import NavBar from './components/layout/NavBar'
+
+import ContactState from './context/contact/ContactState'
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <NavBar />
-        <div className="container">
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-          </Switch>
-        </div>
-      </Fragment>
-    </Router>
+    <ContactState>
+      <Router>
+        <Fragment>
+          <NavBar />
+          <div className="container">
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
   );
 }
 
